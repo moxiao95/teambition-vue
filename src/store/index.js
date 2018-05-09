71173e397f1b1d50032183e6e61cc5c24f9534e7
xiaochuan <18838958405@126.com>
2018-05-09 17:20:46 +0800

    this is my teambition-vue

--- src/store/index.js
+++ src/store/index.js
@@ -2,28 +2,6 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 Vue.use(Vuex);
 
-let hasItemData = [
-    {
-        title:'1',
-        num:1
-    },
-    {
-        title:'2',
-        num:2
-    },
-    {
-        title:'3',
-        num:3
-    },
-    {
-        title:'4',
-        num:4
-    },
-    {
-        title:'5',
-        num:5
-    }
-]
 let datas = JSON.parse(localStorage.getItem('team-list')) || [];
 //组件之间通信数据
 let store = new Vuex.Store({
