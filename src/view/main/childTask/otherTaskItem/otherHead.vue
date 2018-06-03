09522f8bd18e65b62be00d416518123538fc3c5f
xiaochuan <18838958405@126.com>
2018-06-03 20:29:22 +0800

    this is my teambition-vue

--- src/view/main/childTask/otherTaskItem/otherHead.vue
+++ src/view/main/childTask/otherTaskItem/otherHead.vue
@@ -1,5 +1,6 @@
 <template>
 <header class="other-head">
+    <!-- 这是左边的导航 -->
     <nav class="other-head-nav">
         <router-link 
             :to="{path:'/task'}"
@@ -25,6 +26,7 @@
             ></Icon>
         </span>
     </nav>
+    <!-- 这是中间的路由跳转 -->
     <div class="other-head-route">
         <router-link 
             :to="{name:'otherItem',query:{itemId:$route.query.itemId,newItem:true}}" 
@@ -37,6 +39,7 @@
             :class="{'bor-bot-blue':!$route.query.newItem}"
         >文件</router-link>
     </div>
+    <!-- 右侧的设置 -->
     <div class="other-item-set" @click.stop="toSetItem">
         <Icon type="android-settings" size="26"></Icon>
     </div>
